7d9d2af144bc0a8ab31cb571479903bafb993a14
djangofan <djangofan@163.com>
2012-06-07 08:51:43 +0000

    增加测试用例

--- src/test/java/calculator/CalculatorServiceImpl.java
+++ src/test/java/calculator/CalculatorServiceImpl.java
@@ -6,7 +6,7 @@ package calculator;
 public class CalculatorServiceImpl implements CalculatorService {
 
 	public double add(double n1, double n2) {
-		throw new NullPointerException();
+		return n1 + n2;
 	}
 
 	public double subtract(double n1, double n2) {
